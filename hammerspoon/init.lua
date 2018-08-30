7869fb26b2ad24a107bca3ef611a41d3cf8d7a49
Jacopo <beschi.jacopo@gmail.com>
2018-08-30 11:21:53 +0200

    Removes unneeded . from hammerspoon folder

---
+++ hammerspoon/init.lua
@@ -0,0 +1,27 @@
+require "clipboard"
+
+local grid = require "grid"
+
+local mash = {"cmd", "alt" }
+local mashalt = {"cmd", "alt", "ctrl"}
+
+-- window hints
+hs.hints.showTitleThresh = 0
+hs.hotkey.bind(mash, 'i', hs.hints.windowHints)
+
+-- Change screen
+-- TODO Currently not working, need to fix it
+--hs.hotkey.bind(mash, 'o', grid.pushwindow_nextscreen)
+
+-- Snap Window
+hs.hotkey.bind(mash, 'h', grid.snap_west)
+hs.hotkey.bind(mash, 'l', grid.snap_east)
+hs.hotkey.bind(mash, 'k', grid.snap_north)
+hs.hotkey.bind(mash, 'j', grid.snap_south)
+
+hs.hotkey.bind(mashalt, 'h', grid.snap_northwest)
+hs.hotkey.bind(mashalt, 'k', grid.snap_northeast)
+hs.hotkey.bind(mashalt, 'j', grid.snap_southwest)
+hs.hotkey.bind(mashalt, 'l', grid.snap_southeast)
+
+hs.hotkey.bind(mash, 'u', grid.maximize_window)
